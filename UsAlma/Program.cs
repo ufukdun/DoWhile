873965eb24b20dd526e9e7b228517baf6ecd6a97
ufuk.dundar <ufuk.dundar@ALS-YG39506-J03>
2016-11-19 15:14:54 +0200

    Add project files.

---
+++ UsAlma/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Taban Değerini Girin:");
+            int taban = Convert.ToInt32(Console.ReadLine());
+            int us = 0;
+            int sonuc = 1;
+
+            bool durum = true;
+            while (durum)
+            {
+                Console.Write("Us Değerini Girin:");
+                us = Convert.ToInt32(Console.ReadLine());
+
+                if (us <= 0)
+                {
+                    durum = true;
+                }
+                else
+                {
+                    durum = false;
+                }
+            }
+
+            for (int i = 0; i < us; i++)
+            {
+                sonuc *= taban;
+            }
+
+            Console.WriteLine("{0}^{1} = {2}", taban, us, sonuc);
+
+            Console.ReadKey();
+        }
+    }
+}
